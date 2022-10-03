3cfe1212f257e782d040eb3236bcba829e34de18
emicarvalho <emily.carvalho@mercadolivre.com>
2022-10-03 17:13:28 -0300

    ajustando pastas

---
+++ src/aula04/Cliente.java
@@ -0,0 +1,13 @@
+package aula04;
+
+public class Cliente {
+    String ID;
+    String name;
+    String sobrenome;
+
+    public Cliente(String ID, String name, String sobrenome) {
+        this.ID = ID;
+        this.name = name;
+        this.sobrenome = sobrenome;
+    }
+}
---
+++ src/aula04/Fatura.java
@@ -0,0 +1,9 @@
+package aula04;
+
+import java.awt.*;
+
+public class Fatura {
+    private Cliente cliente;
+    private List<Item> listItem;
+    private double valorTotal;
+}
